fe661fea36bec5a91253f18b0922eb882411cf3f
IsnuNasrudin <isnunas@gmail.com>
2024-11-02 21:48:25 +0700

    json

--- database/migrations/2024_10_28_183126_create_questions_table.php
+++ database/migrations/2024_10_28_183126_create_questions_table.php
@@ -14,7 +14,7 @@ return new class extends Migration
         Schema::create('questions', function (Blueprint $table) {
             $table->id();
             $table->text('content');
-            $table->jsonb('options');
+            $table->json('options');
             $table->timestamps();
 
             $table->foreignId('exam_id')->constrained();
